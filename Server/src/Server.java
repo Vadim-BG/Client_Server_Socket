de543298de4d3bc27530c046f54a7eaeb7070f7e
Vadim-BG <infotourbg.vadim@gmail.com>
2020-04-13 17:17:49 +0300

    Version with .Jar Class Phone + MultiThread

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -8,20 +8,35 @@ public class Server {
 
 
         try (ServerSocket server = new ServerSocket(8000)) {
-            System.out.println("Server started");
+            System.out.println("Server started!");
 
-            while (true)
-                try (Phone phone = new Phone(server)) {
-                    String request = phone.readLine();
-                    System.out.println("Request: " + request);
-                    String response = (int) (Math.random() * 30 - 10) + "";
-                    phone.writeLine(response);
-                    System.out.println("Response: " + response);
-                } catch (NullPointerException e) {
+            while (true){
+                Phone phone = new Phone(server);
+
+            new Thread(() -> {
+                String request = phone.readLine();
+                System.out.println("Request: " + request);
+                String response = (int) (Math.random() * 30 - 10) + "";
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+                phone.writeLine(response);
+                System.out.println("Response: " + response);
+                try {
+                    phone.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
         }
+
+    } catch(
+    IOException e)
+
+    {
+        throw new RuntimeException(e);
     }
+}
 }
